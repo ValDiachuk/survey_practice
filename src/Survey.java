bb3bc1aced69aacdb5ff5aefdca54551d3775f50
Valentyna Diachuk <valdiachuk9@gmail.com>
2022-11-16 19:41:28 -0600

    added new project

--- src/Survey.java
+++ src/Survey.java
@@ -1,3 +1,7 @@
+import com.sun.deploy.net.MessageHeader;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Survey {
     public static void main(String[] args) {
@@ -14,6 +18,52 @@ public class Survey {
         -Print the age of the youngest person
         -Print the age of the eldest person
          */
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Participant> participants = new ArrayList<>();
+        int maxAge = 0, minAge = Integer.MAX_VALUE;
+
+        do{
+
+            System.out.println(SurveyQuestions.askToJoin);
+            String answer1 = scanner.nextLine();
+            if (answer1.toLowerCase().startsWith("y")){
+                System.out.println(SurveyQuestions.askName);
+                String name = scanner.nextLine();
+
+
+                System.out.println(SurveyQuestions.askAge);
+                int age = scanner.nextInt();
+                scanner.nextLine();
+
+                if(age > maxAge) maxAge = age;
+
+                if (age < minAge) minAge = age;
+
+
+                System.out.println(SurveyQuestions.askGender);
+                char gender = scanner.nextLine().toUpperCase().charAt(0);
+
+                Participant participant = new Participant(name, age, gender);
+
+                participants.add(participant);
+
+                if (gender == 'M') Participant.addMaleParticipants();
+                else Participant.addFemaleParticipants();
+
+            }
+
+        }while (Participant.totalNumberOfParticipants < 3);
+
+        for (Participant p : participants) {
+            System.out.println(p);
+        }
+        System.out.println("Total number of male participants" + Participant.totalNumberOfMaleParticipants);
+        System.out.println("Total number of female participants" + Participant.totalNumberOfFemaleParticipants);
+
+        System.out.println("The age of youngest person is " + minAge);
+        System.out.println("The age of eldest person is " + maxAge);
 
     }
 }
